305785d21343f067012f0529d19e074cb6b5c157
a8t3r <a.kolosov@evotor.ru>
2018-08-29 15:48:16 +0300

    default constructors

--- src/main/java/io/evotor/market/api/v2/model/group/Attribute.java
+++ src/main/java/io/evotor/market/api/v2/model/group/Attribute.java
@@ -1,13 +1,17 @@
 package io.evotor.market.api.v2.model.group;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Attribute {
 
     private UUID id;
--- src/main/java/io/evotor/market/api/v2/model/group/AttributeChoice.java
+++ src/main/java/io/evotor/market/api/v2/model/group/AttributeChoice.java
@@ -1,12 +1,16 @@
 package io.evotor.market.api.v2.model.group;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttributeChoice {
 
     private UUID id;
